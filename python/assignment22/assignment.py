c62329383c2940f8ec0180e5eb3c0d97301617d8
Tuomas <tuomas.2.makinen@tuni.fi>
2022-11-30 18:31:36 +0200

    Task 1 implementation

--- python/assignment22/assignment.py
+++ python/assignment22/assignment.py
@@ -4,16 +4,28 @@ from typing import List, Tuple
 
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.clustering import KMeansModel
 
 
 class Assignment:
-    spark: SparkSession = None  # REPLACE with actual implementation
+    spark: SparkSession = SparkSession.builder \
+        .appName("ex5") \
+        .config("spark.driver.host", "localhost") \
+        .master("local") \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+    spark.conf.set("spark.sql.shuffle.partitions", "5")
 
     # the data frame to be used in tasks 1 and 4
-    dataD2: DataFrame = None  # REPLACE with actual implementation
+    dataD2: DataFrame = spark.read.options(inferSchema=True, header=True)\
+        .csv("file:/C:/Users/Tuomas/Documents/COMP.CS.320/tuomas/python/data/dataD2.csv")
 
     # the data frame to be used in task 2
-    dataD3: DataFrame = None  # REPLACE with actual implementation
+    dataD3: DataFrame = spark.read.options(inferSchema=True, header=True)\
+        .csv("file:/C:/Users/Tuomas/Documents/COMP.CS.320/tuomas/python/data/dataD3.csv")
 
     # the data frame to be used in task 3 (based on dataD2 but containing numeric labels)
     dataD2WithLabels: DataFrame = None  # REPLACE with actual implementation
@@ -22,7 +34,12 @@ class Assignment:
 
     @staticmethod
     def task1(df: DataFrame, k: int) -> List[Tuple[float, float]]:
-        pass  # REPLACE with actual implementation
+        va: VectorAssembler = VectorAssembler(inputCols=['a', 'b'], outputCol='features')
+        assembledDF: DataFrame = va.transform(df)
+        kmeans: KMeans = KMeans(featuresCol='features', k=k)
+        model: KMeansModel = kmeans.fit(assembledDF)
+        clusters: List[Tuple[float, float]] = list(tuple([tuple(center) for center in model.clusterCenters()]))
+        return clusters
 
     @staticmethod
     def task2(df: DataFrame, k: int) -> List[Tuple[float, float, float]]:

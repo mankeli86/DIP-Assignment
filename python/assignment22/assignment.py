d9cab0fe24bce3171f98a05d6f6b7d2e0795f6e8
Tuomas <tuomas.2.makinen@tuni.fi>
2022-12-03 18:01:15 +0200

    Task 4 implementation, task 3 pipeline

--- python/assignment22/assignment.py
+++ python/assignment22/assignment.py
@@ -4,12 +4,15 @@ from typing import List, Tuple
 
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql import functions
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.clustering import KMeansModel
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.feature import MinMaxScaler
+from pyspark.ml.feature import StandardScaler
 from pyspark.ml import Pipeline, PipelineModel
+from pyspark.ml.evaluation import ClusteringEvaluator
 
 
 class Assignment:
@@ -24,7 +27,7 @@ class Assignment:
 
     # the data frame to be used in tasks 1 and 4
     dataD2: DataFrame = spark.read.options(inferSchema=True, header=True) \
-        .csv("file:/C:/Users/Tuomas/Documents/COMP.CS.320/tuomas/python/data/dataD2.csv")
+        .csv("file:/C:/Users/Tuomas/Documents/COMP.CS.320/tuomas/python/data/dataD2.csv").cache()
 
     # the data frame to be used in task 2
     dataD3: DataFrame = spark.read.options(inferSchema=True, header=True) \
@@ -32,7 +35,7 @@ class Assignment:
 
     # the data frame to be used in task 3 (based on dataD2 but containing numeric labels)
     dataD2WithLabels: DataFrame = StringIndexer(inputCol='LABEL', outputCol='LABEL_NUMERIC')\
-        .fit(dataD2).transform(dataD2)
+        .fit(dataD2).transform(dataD2).cache()
 
     @staticmethod
     def task1(df: DataFrame, k: int) -> List[Tuple[float, float]]:
@@ -57,11 +60,11 @@ class Assignment:
     @staticmethod
     def task3(df: DataFrame, k: int) -> List[Tuple[float, float]]:
         va: VectorAssembler = VectorAssembler(inputCols=['a', 'b', 'LABEL_NUMERIC'], outputCol='features')
-        assembledDF: DataFrame = va.transform(df)
         kmeans: KMeans = KMeans(featuresCol='features', k=k)
-        model: KMeansModel = kmeans.fit(assembledDF)
+        pipeline: Pipeline = Pipeline(stages=[va, kmeans])
+        model: PipelineModel = pipeline.fit(df)
 
-        clusters: List[Tuple[float, float, float]] = list(tuple([tuple(center) for center in model.clusterCenters()]))
+        clusters: List[Tuple[float, float, float]] = list(tuple([tuple(center) for center in model.stages[1].clusterCenters()]))
         clustersTop2Fatal: List[Tuple[float, float]] = \
             [t[:2] for t in sorted(clusters, key=lambda x: x[2], reverse=True)[0:2]]
 
@@ -70,5 +73,17 @@ class Assignment:
     # Parameter low is the lowest k and high is the highest one.
     @staticmethod
     def task4(df: DataFrame, low: int, high: int) -> List[Tuple[int, float]]:
-        pass  # REPLACE with actual implementation
+        scores: List[Tuple[int, float]] = []
+        va: VectorAssembler = VectorAssembler(inputCols=['a', 'b'], outputCol='features')
+        scaler: MinMaxScaler = MinMaxScaler(inputCol="features", outputCol="scaledFeatures")
+        evaluator: ClusteringEvaluator = ClusteringEvaluator(featuresCol="scaledFeatures",
+                                                             metricName='silhouette',
+                                                             distanceMeasure='squaredEuclidean')
+
+        for k in range(low, high + 1):
+            kmeans: KMeans = KMeans(featuresCol="scaledFeatures", k=k, seed=1)
+            pipeline: Pipeline = Pipeline(stages=[va, scaler, kmeans])
+            dfWithPredictions: DataFrame = pipeline.fit(df).transform(df)
+            scores.append((k, evaluator.evaluate(dfWithPredictions)))
+        return scores
 
